2d4fde73ae3819308447a2d47bfe171c3199783b
pinetree <eodnjs568@gmail.com>
2024-01-10 09:46:14 +0900

    feat : ArrayQueue Class resize()메서드 구현
    - 이해가 아직 잘 안되었기에 추후 정리 필요

--- Java/queue/ArrayQueue.java
+++ Java/queue/ArrayQueue.java
@@ -22,6 +22,24 @@ public class ArrayQueue implements Queue {
         rear = 0;
     }
 
+    /**
+     * 멤버변수로 갖고있는 front, rear을 적극활용한다.
+     * TODO: 아직은 이해가 확실히 안되었기에 추후 정리필요
+     * @param capacity 새로 생성할 배열의 크기
+     */
+    private void resize(int capacity) {
+        int arrayCapacity = array.length;
+        Object[] newArray = new Object[capacity];
+
+        for (int i = 1, j = front + 1; i <= size; i++, j++) {
+            newArray[i] = array[j % arrayCapacity];
+        }
+        this.array = null;
+        this.array = newArray;
+        front = 0;
+        rear = size;
+    }
+
     @Override
     public boolean offer(final Object o) {
         return false;
